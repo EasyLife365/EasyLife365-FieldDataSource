e1d2ce23fc9424c7a7246eac2b519a2a3974184b
Patrick Lamber <patrick@nubo.eu>
2022-02-11 07:08:50 +0100

    restructured project

---
+++ EL.FieldDataSource/EL.FieldDataSource/Models/DropdownOption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace EL.FieldDataSource
+{
+    public class DropdownOption
+    {
+        [JsonPropertyName("key")]
+        public String Key { get; set; }
+        [JsonPropertyName("text")]
+        public String Text { get; set; }
+    }
+}
